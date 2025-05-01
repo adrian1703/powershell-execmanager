9e8a232c4391e7d83c12a6dc26788eba984101cf
adrian <adrian.kuhn@arcor.de>
2025-05-01 18:54:06 +0200

    Bump module version to 0.1.0 and regenerate manifest

--- Build/create-manifest.ps1
+++ Build/create-manifest.ps1
@@ -18,12 +18,13 @@ $moduleSettings = @{
     Author = "Adrian Kuhn"
 #    CompanyName = "Adrian Privat"
     PowerShellVersion = '5.0'
-    ModuleVersion = '0.0.0'
+    ModuleVersion = '0.1.0'
     RootModule = 'PSCmdManager.psm1'
     Description = "A Tool for managing a command workflow using yaml files for configuration. A prime
 example would be the deterministic setup of software on a clean instance."
 }
 New-ModuleManifest @moduleSettings
+
 
 
 
