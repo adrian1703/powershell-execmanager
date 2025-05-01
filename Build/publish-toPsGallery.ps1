16500087b2670a6b87f13036539e3b5aecbfde0c
adrian <adrian.kuhn@arcor.de>
2025-05-01 20:07:14 +0200

    bug(build): correct workaround for publishing on win[DE]
    Signed-off-by: adrian <adrian.kuhn@arcor.de>

--- Build/publish-toPsGallery.ps1
+++ Build/publish-toPsGallery.ps1
@@ -1,8 +1,11 @@
+[System.Environment]::SetEnvironmentVariable("NUGET_CLI_LANGUAGE", "en-us", "Process")
+[System.Environment]::SetEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en_US", "Process")
+
 if ($args[0] -eq '-release')
 {
-    Publish-Module -Path ".\PSCmdManager\" -NugetAPIKey "$env:POWERSHELLGALLARY" -Verbose
+    Publish-Module -Path ".\PSCmdManager\" -NuGetApiKey "$env:POWERSHELLGALLARY" -Verbose
 }
 else
 {
-    Publish-Module -Path ".\PSCmdManager\" -NugetAPIKey "$env:POWERSHELLGALLARY" -WhatIf -Verbose
+    Publish-Module -Path ".\PSCmdManager\" -NuGetApiKey "$env:POWERSHELLGALLARY" -WhatIf -Verbose
 }
